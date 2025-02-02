1ddab20c4b2719e3e60974c205d274110fda0307
Sewmini <sewminipremodya98@mail.com>
2025-02-03 03:29:02 +0530

    Updated the CustomerServiceFactory

--- src/main/java/com/example/Vehicle_Reservation_System_Backend/factory/CustomerServiceFactory.java
+++ src/main/java/com/example/Vehicle_Reservation_System_Backend/factory/CustomerServiceFactory.java
@@ -4,8 +4,13 @@ import com.example.Vehicle_Reservation_System_Backend.service.CustomerService;
 import com.example.Vehicle_Reservation_System_Backend.service.impl.CustomerServiceImpl;
 
 public class CustomerServiceFactory {
+    // Singleton instance of CustomerService
+    private static CustomerService customerServiceInstance;
 
     public static CustomerService getCustomerService() {
-        return new CustomerServiceImpl();
+        if (customerServiceInstance == null) {
+            customerServiceInstance = new CustomerServiceImpl();
+        }
+        return customerServiceInstance;
     }
 }
