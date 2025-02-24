8691f288fc361ce740d82b89cf7e7a1321b87bcf
Sewmini <sewminipremodya98@mail.com>
2025-02-24 11:42:29 +0530

    Created the BillingEntity.java, BillingServlet.java files.

---
+++ src/main/java/com/example/Vehicle_Reservation_System_Backend/controller/BillingServlet.java
@@ -0,0 +1,10 @@
+package com.example.Vehicle_Reservation_System_Backend.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/billing")
+public class BillingServlet extends HttpServlet {
+
+
+}
